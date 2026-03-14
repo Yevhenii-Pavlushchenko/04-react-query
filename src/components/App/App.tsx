42b7ce3f0aac3ccb63249e55d47fe977c1ecd3db
JustFun <paw93@ukr.net>
2026-03-14 11:16:47 +0200

    add  css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -72,11 +72,13 @@ export default function App() {
           marginPagesDisplayed={1}
           onPageChange={({ selected }) => {
             setPage(selected + 1);
-
           }}
           forcePage={page - 1}
+
           containerClassName={css.pagination}
           activeClassName={css.active}
+          pageClassName={css.pageItem}
+          
           nextLabel="→"
           previousLabel="←"
         />
